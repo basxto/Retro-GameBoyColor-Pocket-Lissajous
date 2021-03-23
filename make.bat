489a5d29cc96fd3d5cdab901ce64ee38ab7f3056
Sebastian Riedel <git@basxto.de>
2021-03-23 23:24:10 +0100

    Add bash script for unixoids

--- make.bat
+++ make.bat
@@ -1,3 +1,4 @@
-rgbasm -opocketl.obj pocketl.s
-rgblink -mpocketl.map -npocketl.sym -opocketl.gb pocketl.obj
-rgbfix -v -p0 pocketl.gb
+set ROM=pocketl
+rgbasm -o%ROM%.obj %ROM%.s
+rgblink -m%ROM%.map -n%ROM%.sym -o%ROM%.gb %ROM%.obj
+rgbfix -v -p0 %ROM%.gb
---
+++ make.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ROM=pocketl
+rgbasm -o${ROM}.obj ${ROM}.s
+rgblink -m${ROM}.map -n${ROM}.sym -o${ROM}.gb ${ROM}.obj
+rgbfix -v -p0 ${ROM}.gb
